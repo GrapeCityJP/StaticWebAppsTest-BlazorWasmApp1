20cd383063c205023e9b292d42aa7e9ce86e1dc5
Kuni <kuni.ujiie@grapecity.com>
2022-08-02 17:42:47 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorWasmApp1/Data/Album.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorWasmApp1.Data
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Artist { get; set; }
+
+        public double Price { get; set; }
+
+        public string? Image_url { get; set; }
+
+    }
+}
